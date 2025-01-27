4884383b2c4eeb3c138b97d7ce4470da0b26b571
jdMary <mariem.jrad@esprit.tn>
2025-01-27 11:37:16 +0100

    class 2nd session

---
+++ src/main/java/com/example/springcrashcourses/SpringCrashCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrashcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrashCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrashCoursesApplication.class, args);
+    }
+
+}
